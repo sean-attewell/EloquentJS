eae23ce82da62d105b40d15d178994695bf22c2e
sean-attewell <sean.attewell@gmail.com>
2019-08-21 18:28:11 +0100

    more exercises

--- chapter_04_exercises.js
+++ chapter_04_exercises.js
@@ -1,10 +1,12 @@
-function range(start, end, step) {
-  /////////////////////////// U R HERE
-  let arr = [];
-  for (let i = start; i <= end; i++) {
-    arr.push(i);
+function range(start, end, step = start < end ? 1 : -1) {
+  let array = [];
+
+  if (step > 0) {
+    for (let i = start; i <= end; i += step) array.push(i);
+  } else {
+    for (let i = start; i >= end; i += step) array.push(i);
   }
-  return arr;
+  return array;
 }
 
 console.log(range(3, 9));
@@ -17,4 +19,35 @@ function sum(arrayOfNums) {
   return total;
 }
 
+console.log(range(1, 10));
+// → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(range(5, 2, -1));
+// → [5, 4, 3, 2]
 console.log(sum(range(1, 10)));
+// → 55
+
+// REVERSING AN ARRAY
+
+function reverseArray(arr) {
+  let newArr = [];
+  for (let item of arr) {
+    newArr.unshift(item);
+  }
+  return newArr;
+}
+console.log(reverseArray(['A', 'B', 'C']));
+// → ["C", "B", "A"];
+
+function reverseArrayInPlace(array) {
+  for (let i = 0; i < Math.floor(array.length / 2); i++) {
+    let old = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = old;
+  }
+  return array;
+}
+
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+// → [5, 4, 3, 2, 1]
