bdaff331323ca4e0f877221c8f2c8ab968b7d18f
sean-attewell <sean.attewell@gmail.com>
2019-09-24 15:52:13 +0100

    ch 16 fin

--- 16_game.js
+++ 16_game.js
@@ -319,6 +319,44 @@ Player.prototype.update = function(time, state, keys) {
   return new Player(pos, new Vec(xSpeed, ySpeed));
 };
 
+const monsterSpeed = 4;
+
+class Monster {
+  constructor(pos) {
+    this.pos = pos;
+  }
+
+  get type() {
+    return 'monster';
+  }
+
+  static create(pos) {
+    return new Monster(pos.plus(new Vec(0, -1)));
+  }
+
+  update(time, state) {
+    let player = state.player;
+    let speed = (player.pos.x < this.pos.x ? -1 : 1) * time * monsterSpeed;
+    let newPos = new Vec(this.pos.x + speed, this.pos.y);
+    if (state.level.touches(newPos, this.size, 'wall')) return this;
+    else return new Monster(newPos);
+  }
+
+  collide(state) {
+    let player = state.player;
+    if (player.pos.y + player.size.y < this.pos.y + 0.5) {
+      let filtered = state.actors.filter(a => a != this);
+      return new State(state.level, filtered, state.status);
+    } else {
+      return new State(state.level, state.actors, 'lost');
+    }
+  }
+}
+
+Monster.prototype.size = new Vec(1.2, 2);
+
+levelChars['M'] = Monster;
+
 function trackKeys(keys) {
   let down = Object.create(null);
   function track(event) {
@@ -329,6 +367,10 @@ function trackKeys(keys) {
   }
   window.addEventListener('keydown', track);
   window.addEventListener('keyup', track);
+  down.unregister = () => {
+    window.removeEventListener('keydown', track);
+    window.removeEventListener('keyup', track);
+  };
   return down;
 }
 
@@ -347,12 +389,50 @@ function runAnimation(frameFunc) {
   requestAnimationFrame(frame);
 }
 
+// To know when to stop and restart the animation, a level that is
+// being displayed may be in three `running` states:
+//
+// * "yes":     Running normally.
+// * "no":      Paused, animation isn't running
+// * "pausing": Must pause, but animation is still running
+//
+// The key handler, when it notices escape being pressed, will do a
+// different thing depending on the current state. When running is
+// "yes" or "pausing", it will switch to the other of those two
+// states. When it is "no", it will restart the animation and switch
+// the state to "yes".
+//
+// The animation function, when state is "pausing", will set the state
+// to "no" and return false to stop the animation.
+
 function runLevel(level, Display) {
   let display = new Display(document.body, level);
   let state = State.start(level);
   let ending = 1;
+  let running = 'yes';
+
   return new Promise(resolve => {
-    runAnimation(time => {
+    function escHandler(event) {
+      if (event.key != 'Escape') return;
+      event.preventDefault();
+      if (running == 'no') {
+        running = 'yes';
+        runAnimation(frame);
+      } else if (running == 'yes') {
+        running = 'pausing';
+      } else {
+        running = 'yes';
+      }
+    }
+    window.addEventListener('keydown', escHandler);
+    let arrowKeys = trackKeys(['ArrowLeft', 'ArrowRight', 'ArrowUp']);
+
+    function frame(time) {
+      if (running == 'pausing') {
+        running = 'no';
+        return false;
+      }
+
       state = state.update(time, arrowKeys);
       display.syncState(state);
       if (state.status == 'playing') {
@@ -362,17 +442,28 @@ function runLevel(level, Display) {
         return true;
       } else {
         display.clear();
+        window.removeEventListener('keydown', escHandler);
+        arrowKeys.unregister();
         resolve(state.status);
         return false;
       }
-    });
+    }
+    runAnimation(frame);
   });
 }
 
 async function runGame(plans, Display) {
-  for (let level = 0; level < plans.length; ) {
+  let lives = 3;
+  for (let level = 0; level < plans.length && lives > 0; ) {
+    console.log(`Level ${level + 1}, lives: ${lives}`);
     let status = await runLevel(new Level(plans[level]), Display);
     if (status == 'won') level++;
+    else lives--;
   }
-  console.log("You've won!");
+  if (lives > 0) {
+    console.log("You've won!");
+  } else {
+    console.log('Game over');
+  }
 }
+runGame(GAME_LEVELS, DOMDisplay);
